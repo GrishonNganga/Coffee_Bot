feb906007c1c48eed1e8f9077136f3fe4d40a037
Velda Kiara <veldakarimi@gmail.com>
2020-05-11 15:00:36 +0300

    updated size changes

--- coffee_bot.py
+++ coffee_bot.py
@@ -1,7 +1,11 @@
 # Define your functions
 def coffee_bot():
-  return "Welcome to the Cafe"
-
+    return "Welcome to the Cafe"
+def get_size(drink):
+    res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
+    return res
 
 # Call coffee_bot()!
 print(coffee_bot())
+size = get_size(1)
+print(size)
