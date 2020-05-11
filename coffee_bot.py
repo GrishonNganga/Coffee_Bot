c7fe3ebfa8dfa0f980445c71829d99a363dbf11a
Velda Kiara <veldakarimi@gmail.com>
2020-05-12 01:31:55 +0300

    added  lattes and edited the drink type

--- coffee_bot.py
+++ coffee_bot.py
@@ -1,14 +1,14 @@
 # Define your functions
 def coffee_bot():
   size = get_size()
-  print(size)
   drink_type = get_drink_type()
-  print(drink_type)
+  print("Alright, that\'s a {} {}!".format(size, drink_type))
   return "Welcome to the Cafe"
 
   
 def print_message():
     return "I'm sorry, I did not understand your selection. Please enter the corresponding letter for your response."
+
 def get_size():
     res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
     if res == "a":
@@ -28,7 +28,7 @@ def get_drink_type():
   elif res == "b":
     return "mocha"
   elif res == "c":
-    return "latte"
+    return order_latte()
   else:
     print_message()
     return get_drink_type
